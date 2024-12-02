2365ef62c3e3ad14cdf15622a7b03546d5daaccb
Efthymios Hadjimichael <ehadjimichael@id5.io>
2024-12-02 19:32:45 +0200

    Add 2024 2

---
+++ 2024/src/advent/d02.rs
@@ -0,0 +1,44 @@
+use crate::advent::util::parse;
+use nom::{bytes::complete::tag, character::complete::u32, multi::separated_list0, IResult};
+
+fn parse_reports(input: &str) -> IResult<&str, Vec<Vec<u32>>> {
+    let (remainder, v) = separated_list0(tag("\n"), separated_list0(tag(" "), u32))(input)?;
+    Ok((remainder, v))
+}
+
+fn is_valid_report(ve: &Vec<u32>) -> bool {
+    let increasing = ve.iter().is_sorted_by(|&a, &b| a < b && b - a <= 3);
+    let decreasing = ve.iter().is_sorted_by(|&a, &b| a > b && a - b <= 3);
+    increasing || decreasing
+}
+
+pub fn pt1(path: String) -> Result<(), Box<dyn std::error::Error>> {
+    let v: Vec<Vec<u32>> = parse::with_nom(&path, parse_reports)?;
+
+    let amt_safe = v.iter().filter(|ve| is_valid_report(ve)).count();
+    println!("Safe reports: {}", amt_safe);
+    Ok(())
+}
+
+pub fn pt2(path: String) -> Result<(), Box<dyn std::error::Error>> {
+    let v: Vec<Vec<u32>> = parse::with_nom(&path, parse_reports)?;
+
+    let amt_safe = v
+        .iter()
+        .filter(|&ve| {
+            if is_valid_report(ve) {
+                return true;
+            }
+            for i in 0..ve.len() {
+                let mut vr = ve.clone();
+                vr.remove(i);
+                if is_valid_report(&vr) {
+                    return true;
+                }
+            }
+            return false;
+        })
+        .count();
+    println!("Safe reports: {}", amt_safe);
+    Ok(())
+}
--- 2024/src/advent/mod.rs
+++ 2024/src/advent/mod.rs
@@ -1,10 +1,13 @@
 mod util;
 mod d01;
+mod d02;
 
 pub fn run (exercise: u8, part: u8, file: String) -> Result<(), Box<dyn std::error::Error>> {
     match (exercise, part) {
         (1,1) => { d01::pt1(file) }
         (1,2) => { d01::pt2(file) }
+        (2,1) => { d02::pt1(file) }
+        (2,2) => { d02::pt2(file) }
         _ => { print!("No such exercise found: {}, pt{}", exercise, part); Ok(()) }
     }
 }
