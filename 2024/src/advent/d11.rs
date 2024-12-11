bc33612d971fee176256924e2985652529f88582
Efthymios Hadjimichael <ehadjimichael@id5.io>
2024-12-11 18:24:17 +0200

    Prep for 2024 11 pt2

--- 2024/src/advent/d11.rs
+++ 2024/src/advent/d11.rs
@@ -5,31 +5,38 @@ use nom::{bytes::complete::tag, IResult};
 
 fn parse_stones(input: &str) -> IResult<&str, Vec<u64>> {
     let (remainder, v) = separated_list0(tag(" "), u64)(input)?;
-    Ok((
-        remainder,
-        v
-    ))
+    Ok((remainder, v))
+}
+
+fn blink(mut v: Vec<u64>, blinks: usize) {
+    for _i in 0..blinks {
+        v = v
+            .iter()
+            .flat_map(|&stone| {
+                if stone == 0 {
+                    return vec![1];
+                }
+                let s = stone.to_string();
+                if s.len() % 2 == 0 {
+                    let (ls, rs) = s.split_at(s.len() / 2);
+                    vec![
+                        u64::from_str_radix(ls, 10).unwrap(),
+                        u64::from_str_radix(rs, 10).unwrap(),
+                    ]
+                } else {
+                    vec![stone * 2024]
+                }
+            })
+            .collect();
+        println!("Performed {} iterations", _i);
+    }
+    println!("Amount of stones: {}", v.len());
 }
 
 pub fn pt1(path: String) -> Result<(), Box<dyn std::error::Error>> {
-    let mut v: Vec<u64> = util::parse::with_nom(&path, parse_stones)?;
+    let v: Vec<u64> = util::parse::with_nom(&path, parse_stones)?;
 
-    for _i in 0..25 {
-        v = v.iter().flat_map(|&stone|{
-            let s = stone.to_string();
-            if stone == 0 {
-                vec![1]
-            }
-            else if s.len() % 2 ==0 {
-                let (ls, rs) = s.split_at(s.len() / 2);
-                vec![u64::from_str_radix(ls, 10).unwrap(), u64::from_str_radix(rs, 10).unwrap()]
-            } else {
-                vec![stone * 2024]
-            }
-        }).collect();
-        println!("After {} iterations: {:?}", _i, v);
-    }
-    println!("Amount of stones: {}", v.len());
+    blink(v, 25);
 
     Ok(())
 }
@@ -37,7 +44,7 @@ pub fn pt1(path: String) -> Result<(), Box<dyn std::error::Error>> {
 pub fn pt2(path: String) -> Result<(), Box<dyn std::error::Error>> {
     let v: Vec<u64> = util::parse::with_nom(&path, parse_stones)?;
 
-    println!("Amount of stones: {}", v.len());
+    blink(v, 75);
 
     Ok(())
 }
