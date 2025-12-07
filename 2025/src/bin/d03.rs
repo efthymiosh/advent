f85a53a22d4d32df3dd4d53f614a1244aa0df8dd
Efthymios Hadjimichael <efthymios.hadjimichael@keragon.com>
2025-12-07 11:49:09 +0200

    Extract d03 functionality to function

--- 2025/src/bin/d03.rs
+++ 2025/src/bin/d03.rs
@@ -14,21 +14,28 @@ fn parse_lists(input: &str) -> IResult<&str, Vec<Vec<u32>>> {
     ))
 }
 
+fn battery_jolt(input: Vec<u32>, _amount: usize) -> Result<u32, Box<dyn std::error::Error>> {
+    // input: the battery vector to examine
+    // amount: the amount of batteries
+    let max1_pos = input[..input.len() - 1].iter().enumerate().fold(0, |s, (idx, e)|{
+        if *e > input[s] {
+            idx
+        } else {
+            s
+        }
+    });
+    let max2 = input[max1_pos + 1..].iter().max().ok_or("No max")?;
+    let num = input[max1_pos] * 10 + max2;
+    println!("Jolt: {num}");
+    return Ok(num)
+
+}
+
 pub fn pt1(path: String) -> Result<(), Box<dyn std::error::Error>> {
     let v: Vec<Vec<u32>> = util::parse::with_nom(&path, parse_lists)?;
     let mut sum = 0;
     for ve in v {
-        let max1_pos = ve[..ve.len() - 1].iter().enumerate().fold(0, |s, (idx, e)|{
-            if *e > ve[s] {
-                idx
-            } else {
-                s
-            }
-        });
-        let max2 = ve[max1_pos + 1..].iter().max().ok_or("No max")?;
-        let num = ve[max1_pos] * 10 + max2;
-        println!("Jolt: {num}");
-        sum += num;
+        sum += battery_jolt(ve, 2)?;
     }
     println!("Sum of jolts: {sum}");
     Ok(())
